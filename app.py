be80e177aefcd59c621b0f12cf1e81ff75d3f30d
Gaurav <gmaurya783@gmail.com>
2022-09-30 17:46:06 +0530

    test13

--- app.py
+++ app.py
@@ -32,7 +32,38 @@ def down(img_url, filename):
         f.write(r.content)
 
 
+# Video Generating function
+@app.route('/vdo')
+def generate_video():
+    image_folder = '.' # make sure to use your folder
+    video_name = 'video.avi'
+    os.chdir(path)
+    
+    images = [img for img in os.listdir(image_folder)
+            if img.endswith(".jpg") or
+                img.endswith(".jpeg") or
+                img.endswith("png")]
+    
+    # Array images should only consider
+    # the image files ignoring others if any
+    print(images)
 
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+
+    # setting the frame width, height width
+    # the width, height of first image
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(video_name, 0, 1, (width, height))
+
+    # Appending the images to the video one by one
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+    
+    # Deallocating memories taken for window creation
+    cv2.destroyAllWindows()
+    video.release() # releasing the video generated
+    return "video generated"
 
 
 @app.route('/img')
